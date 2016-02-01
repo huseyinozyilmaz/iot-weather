ad6dac11495ed28d31a50713e4a9dbfed4e4e9b8
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2016-02-01 19:14:15 +0000

    Reads the actual sensor

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(server);
 var url = require('url');
 var ip = require('ip');
 var os = require("os");
+var sensorlib = require('node-dht-sensor');
 
 var config = { 
     protocol: 'http:',
@@ -28,6 +29,42 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
     var delay = 5000;
 
+    var sensor = {
+        initialize: function () {
+            return sensorlib.initialize(22, 4);
+        },
+        read: function () {
+            var readout = sensorlib.read();
+            var timestamp = new Date();
+            var readings = {
+                id: os.hostname(),
+                temperature : {
+                    value: readout.temperature,
+                    unit: "°C",
+                    timestamp: timestamp
+                },
+                humidity : {
+                    value: readout.humidity,
+                    unit: "%",
+                    timestamp: timestamp
+                }
+            };
+            console.log("T:" + readout.temperature.toFixed(2) + " | H:" + eadout.humidity.toFixed(2));
+            socket.send(readings); 
+            setTimeout(function () {
+                sensor.read();
+            }, delay);
+        }
+    };
+
+    if (sensor.initialize()) {
+        sensor.read();
+    } else {
+        console.warn('Failed to initialize sensor');
+        socket.send({error:"Failed to initialize sensor"}); 
+    }
+
+    /**
     (function sendData() {
         var readings = {
             id: os.hostname(),
@@ -47,7 +84,7 @@ io.on('connection', function (socket) {
         
         setTimeout(sendData, delay);
     })();
-    
+    */
 });
 
 function random (low, high) {
